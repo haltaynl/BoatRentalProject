a14514777281a1bded3605aebf97571fc0b43633
Altay <hatice.altay@capgemini.com>
2024-05-01 13:44:19 +0200

    hatice updateboat

--- boatRental/src/main/java/org/capgemini/view/UpdateBoatView.java
+++ boatRental/src/main/java/org/capgemini/view/UpdateBoatView.java
@@ -33,30 +33,35 @@ public class UpdateBoatView {
                     System.out.print("New Boat Name: ");
                     String newBoatName = scanner.nextLine();
                     update_boat.setBoatName(newBoatName);
+                    System.out.println("The Boat Name is updated. ");
                     break;
                 case "2":
                     scanner.nextLine();
                     System.out.print("Seats: ");
                     int newSeats = scanner.nextInt();
                     update_boat.setSeats(newSeats);
+                    System.out.println("The Boat Seats is updated. ");
                     break;
                 case "3":
                     scanner.nextLine();
                     System.out.print("Charging Time: ");
                     int newChargingTime = scanner.nextInt();
                     update_boat.setChargingTime(newChargingTime);
+                    System.out.println("The Boat ChargingTime is updated. ");
                     break ;
                 case "4":
                     scanner.nextLine();
                     System.out.print("Price: ");
                     double price= scanner.nextDouble();
                     update_boat.setPrice(price);
+                    System.out.println("The Price is updated. ");
                     break;
                 case "5":
                     scanner.nextLine();
                     System.out.print("Availability: ");
                     boolean availability= scanner.nextBoolean();
                     update_boat.setAvailability(availability);
+                    System.out.println("The Boat Availability is updated. ");
                     break;
                 case "9":
 
